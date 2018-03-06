f42620c3a7a76b88beaa068076e35f89f002dcef
Francis Williams <francis@fwilliams.info>
2018-03-05 22:13:12 -0500

    follow libIGL style guidelines

--- include/igl/marching_tets.cpp
+++ include/igl/marching_tets.cpp
@@ -7,146 +7,160 @@
 #include <iostream>
 
 
-inline std::int64_t make_edge_key(const std::pair<std::int32_t, std::int32_t>& p) {
-    std::int64_t ret = 0;
-    ret |= p.first;
-    ret |= static_cast<std::int64_t>(p.second) << 32;
-    return ret;
-}
-
 template <typename DerivedTV,
           typename DerivedTT,
           typename Derivedisovalues,
           typename DerivedoutV,
           typename DerivedoutF>
 void igl::marching_tets(
-        const Eigen::PlainObjectBase<DerivedTV>& TV,
-        const Eigen::PlainObjectBase<DerivedTT>& TT,
-        const Eigen::PlainObjectBase<Derivedisovalues>& isovals,
-        double isovalue,
-        Eigen::PlainObjectBase<DerivedoutV>& outV,
-        Eigen::PlainObjectBase<DerivedoutF>& outF) {
-    using namespace std;
+    const Eigen::PlainObjectBase<DerivedTV>& TV,
+    const Eigen::PlainObjectBase<DerivedTT>& TT,
+    const Eigen::PlainObjectBase<Derivedisovalues>& isovals,
+    double isovalue,
+    Eigen::PlainObjectBase<DerivedoutV>& outV,
+    Eigen::PlainObjectBase<DerivedoutF>& outF)
+{
+  using namespace std;
 
-    const int mt_cell_lookup[16][4] = {
-        { -1, -1, -1, -1 }, // 0000 0a
-        {  0,  2,  1, -1 }, // 0001 1a
-        {  0,  3,  4, -1 }, // 0010 1b
-        {  2,  1,  3,  4 }, // 0011 2b
-        {  5,  3,  1, -1 }, // 0100 1d
-        {  0,  2,  5,  3 }, // 0101 2a
-        {  0,  1,  5,  4 }, // 0110 2d
-        {  2,  5,  4, -1 }, // 0111 3a
-        {  4,  5,  2, -1 }, // 1000 1c
-        {  0,  4,  5,  1 }, // 1001 2c
-        {  0,  3,  5,  2 }, // 1010 2f
-        {  1,  3,  5, -1 }, // 1011 3d
-        {  4,  3,  1,  2 }, // 1100 2e
-        {  0,  4,  3, -1 }, // 1101 3c
-        {  0,  1,  2, -1 }, // 1110 3b
-        { -1, -1, -1, -1 }, // 1111 0b
-    };
+  const auto make_edge_key = [](const pair<int32_t, int32_t>& p) -> int64_t
+  {
+    static_assert(sizeof(std::int64_t) == 2*sizeof(std::int32_t),
+                  "need to fit 2 ints into a size_t");
+    std::int64_t ret = 0;
+    ret |= p.first;
+    ret |= static_cast<std::int64_t>(p.second) << 32;
+    return ret;
+  };
 
-    const int mt_edge_lookup[6][2] = {
-        {0, 1},
-        {0, 2},
-        {0, 3},
-        {1, 2},
-        {1, 3},
-        {2, 3},
-    };
+  const int mt_cell_lookup[16][4] =
+  {
+    { -1, -1, -1, -1 },
+    {  0,  2,  1, -1 },
+    {  0,  3,  4, -1 },
+    {  2,  1,  3,  4 },
+    {  5,  3,  1, -1 },
+    {  0,  2,  5,  3 },
+    {  0,  1,  5,  4 },
+    {  2,  5,  4, -1 },
+    {  4,  5,  2, -1 },
+    {  0,  4,  5,  1 },
+    {  0,  3,  5,  2 },
+    {  1,  3,  5, -1 },
+    {  4,  3,  1,  2 },
+    {  0,  4,  3, -1 },
+    {  0,  1,  2, -1 },
+    { -1, -1, -1, -1 },
+  };
 
-    vector<Eigen::RowVector3d> vertices;
-    vector<Eigen::RowVector3i> faces;
-    vector<pair<int, int>> edge_table;
+  const int mt_edge_lookup[6][2] =
+  {
+    {0, 1},
+    {0, 2},
+    {0, 3},
+    {1, 2},
+    {1, 3},
+    {2, 3},
+  };
+
+  vector<Eigen::RowVector3d> vertices;
+  vector<Eigen::RowVector3i> faces;
+  vector<pair<int, int>> edge_table;
 
 
-    assert(TT.cols() == 4 && TT.rows() >= 1);
-    assert(TV.cols() == 3 && TV.rows() >= 4);
-    assert(isovals.cols() == 1);
+  assert(TT.cols() == 4 && TT.rows() >= 1);
+  assert(TV.cols() == 3 && TV.rows() >= 4);
+  assert(isovals.cols() == 1);
 
-    static_assert(sizeof(std::int64_t) == 2*sizeof(std::int32_t), "need to fit 2 ints into a size_t");
-
-    // For each tet
-    for (int i = 0; i < TT.rows(); i++) {
-        uint8_t key = 0;
-        for (int v = 0; v < 4; v++) {
-            int vid = TT(i, v);
-            uint8_t flag = isovals[vid] > isovalue;
-            key |= flag << v;
-        }
-
-        // Insert the vertices if they don't exist
-        int v_ids[4] = {-1, -1, -1, -1}; // This will contain the index in TV of each vertex in the tet
-        for (int e = 0; mt_cell_lookup[key][e] != -1 && e < 4; e++) {
-            const int v1id = mt_edge_lookup[mt_cell_lookup[key][e]][0];
-            const int v2id = mt_edge_lookup[mt_cell_lookup[key][e]][1];
-            const int v1i = TT(i, v1id), v2i = TT(i, v2id);
-            const Eigen::RowVector3d v1 = TV.row(v1i);
-            const Eigen::RowVector3d v2 = TV.row(v2i);
-            const double a = fabs(isovals[v1i] - isovalue), b = fabs(isovals[v2i] - isovalue);
-            const double w = a / (a+b);
-
-            const int vertex_id = vertices.size();
-            vertices.push_back((1-w)*v1 + w*v2); // Push back the midpoint
-            if (v1i < v2i) {
-                edge_table.push_back(make_pair(v1i, v2i));
-            } else {
-                edge_table.push_back(make_pair(v2i, v1i));
-            }
-
-            v_ids[e] = vertex_id;
-        }
-
-        if (v_ids[0] != -1) {
-            bool is_quad = mt_cell_lookup[key][3] != -1;
-            if (is_quad) {
-                Eigen::RowVector3i f1(v_ids[0], v_ids[1], v_ids[3]);
-                Eigen::RowVector3i f2(v_ids[1], v_ids[2], v_ids[3]);
-                faces.push_back(f1);
-                faces.push_back(f2);
-            } else {
-                Eigen::RowVector3i f(v_ids[0], v_ids[1], v_ids[2]);
-                faces.push_back(f);
-            }
-        }
+  // For each tet
+  for (int i = 0; i < TT.rows(); i++)
+  {
+    uint8_t key = 0;
+    for (int v = 0; v < 4; v++)
+    {
+      int vid = TT(i, v);
+      uint8_t flag = isovals[vid] > isovalue;
+      key |= flag << v;
     }
 
-    // Deduplicate vertices
-    int num_unique = 0;
-    outV.resize(vertices.size(), 3);
-    outF.resize(faces.size(), 3);
-    unordered_map<int64_t, int> emap;
-    for (int i = 0; i < faces.size(); i++) {
-        for (int v = 0; v < 3; v++) {
-            const int vi = faces[i][v];
-            const pair<int32_t, int32_t> edge = edge_table[vi];
-            const int64_t key = make_edge_key(edge);
-            auto it = emap.find(key);
-            if (it == emap.end()) { // New unique vertex, insert it
-                outV.row(num_unique) = vertices[vi];
-                outF(i, v) = num_unique;
-                emap.insert(make_pair(key, num_unique));
-                num_unique += 1;
-            } else {
-                outF(i, v) = it->second;
-            }
-        }
+
+    // This will contain the index in TV of each vertex in the tet
+    int v_ids[4] = {-1, -1, -1, -1};
+
+    // Insert any vertices if the tet intersects the level surface
+    for (int e = 0; mt_cell_lookup[key][e] != -1 && e < 4; e++)
+    {
+      const int v1_idx = TT(i, mt_edge_lookup[mt_cell_lookup[key][e]][0]);
+      const int v2_idx = TT(i,  mt_edge_lookup[mt_cell_lookup[key][e]][1]);
+      const Eigen::RowVector3d v1 = TV.row(v1_idx);
+      const Eigen::RowVector3d v2 = TV.row(v2_idx);
+
+      // Linearly interpolate between tet vertices along edges
+      const double a = fabs(isovals[v1_idx] - isovalue);
+      const double b = fabs(isovals[v2_idx] - isovalue);
+      const double w = a / (a+b);
+
+      const int vertex_id = vertices.size();
+      vertices.push_back((1-w)*v1 + w*v2);
+      if (v1_idx < v2_idx)
+      {
+        edge_table.push_back(make_pair(v1_idx, v2_idx));
+      }
+      else
+      {
+        edge_table.push_back(make_pair(v2_idx, v1_idx));
+      }
+
+      v_ids[e] = vertex_id;
     }
-    outV.conservativeResize(num_unique, 3);
+
+    // Insert the corresponding faces
+    if (v_ids[0] != -1)
+    {
+      bool is_quad = mt_cell_lookup[key][3] != -1;
+      if (is_quad)
+      {
+        Eigen::RowVector3i f1(v_ids[0], v_ids[1], v_ids[3]);
+        Eigen::RowVector3i f2(v_ids[1], v_ids[2], v_ids[3]);
+        faces.push_back(f1);
+        faces.push_back(f2);
+      }
+      else
+      {
+        Eigen::RowVector3i f(v_ids[0], v_ids[1], v_ids[2]);
+        faces.push_back(f);
+      }
+    }
+  }
+
+  // Deduplicate vertices
+  int num_unique = 0;
+  outV.resize(vertices.size(), 3);
+  outF.resize(faces.size(), 3);
+  unordered_map<int64_t, int> emap;
+  for (int i = 0; i < faces.size(); i++)
+  {
+    for (int v = 0; v < 3; v++)
+    {
+      const int vi = faces[i][v];
+      const pair<int32_t, int32_t> edge = edge_table[vi];
+      const int64_t key = make_edge_key(edge);
+      auto it = emap.find(key);
+      if (it == emap.end()) // New unique vertex, insert it
+      {
+        outV.row(num_unique) = vertices[vi];
+        outF(i, v) = num_unique;
+        emap.insert(make_pair(key, num_unique));
+        num_unique += 1;
+      }
+      else
+      {
+        outF(i, v) = it->second;
+      }
+    }
+  }
+  outV.conservativeResize(num_unique, 3);
 }
 
 #ifdef IGL_STATIC_LIBRARY
-template void igl::marching_tets<typename Eigen::Matrix<double, -1, -1, 0, -1, -1>,
-                                 typename Eigen::Matrix<int, -1, -1, 0, -1, -1>,
-                                 typename Eigen::Matrix<double, -1, 1, 0, -1, 1>,
-                                 typename Eigen::Matrix<double, -1, -1, 0, -1, -1>,
-                                 typename Eigen::Matrix<int, -1, -1, 0, -1, -1>>(
-        Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&,
-        Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>> const&,
-        const Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1>>&,
-        double isovalue,
-        Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>>&,
-        Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&);
-
+template void igl::marching_tets<typename Eigen::Matrix<double, -1, -1, 0, -1, -1>, typename Eigen::Matrix<int, -1, -1, 0, -1, -1>, typename Eigen::Matrix<double, -1, 1, 0, -1, 1>, typename Eigen::Matrix<double, -1, -1, 0, -1, -1>, typename Eigen::Matrix<int, -1, -1, 0, -1, -1>>(Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>> const&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>> const&, const Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1>>&, double isovalue, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, -1, 0, -1, -1>>&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, -1, 0, -1, -1>>&);
 #endif // IGL_STATIC_LIBRARY
--- tutorial/713_MarchingTetrahedra/main.cpp
+++ tutorial/713_MarchingTetrahedra/main.cpp
@@ -24,60 +24,74 @@ Eigen::MatrixXi isoF;
 
 
 // Visualize the tet mesh as a wireframe
-void visualize_tet_wireframe(igl::opengl::glfw::Viewer& viewer, const Eigen::MatrixXd& TV, const Eigen::MatrixXi& TT, const Eigen::VectorXd& isovals) {
-    std::vector<std::pair<int, int>> edges;
-    for (int i = 0; i < TT.rows(); i++) {
-        int tf1 = TT(i, 0);
-        int tf2 = TT(i, 1);
-        int tf3 = TT(i, 2);
-        int tf4 = TT(i, 2);
-        edges.push_back(std::make_pair(tf1, tf2));
-        edges.push_back(std::make_pair(tf1, tf3));
-        edges.push_back(std::make_pair(tf1, tf4));
-        edges.push_back(std::make_pair(tf2, tf3));
-        edges.push_back(std::make_pair(tf2, tf4));
-        edges.push_back(std::make_pair(tf3, tf4));
-    }
+void visualize_tet_wireframe(igl::opengl::glfw::Viewer& viewer,
+                             const Eigen::MatrixXd& TV,
+                             const Eigen::MatrixXi& TT,
+                             const Eigen::VectorXd& isovals) {
+  using namespace Eigen;
 
-    Eigen::MatrixXd v1(edges.size(), 3), v2(edges.size(), 3);
-    for (int i = 0; i < edges.size(); i++) {
-        v1.row(i) = TV.row(edges[i].first);
-        v2.row(i) = TV.row(edges[i].second);
-    }
+  // Make a black line for each edge in the tet mesh which we'll draw
+  std::vector<std::pair<int, int>> edges;
+  for (int i = 0; i < TT.rows(); i++) {
+    int tf1 = TT(i, 0);
+    int tf2 = TT(i, 1);
+    int tf3 = TT(i, 2);
+    int tf4 = TT(i, 2);
+    edges.push_back(std::make_pair(tf1, tf2));
+    edges.push_back(std::make_pair(tf1, tf3));
+    edges.push_back(std::make_pair(tf1, tf4));
+    edges.push_back(std::make_pair(tf2, tf3));
+    edges.push_back(std::make_pair(tf2, tf4));
+    edges.push_back(std::make_pair(tf3, tf4));
+  }
 
-    Eigen::MatrixXd C;
-    Eigen::VectorXd isovals_normalized = (isovals - isovals.minCoeff() * Eigen::VectorXd::Ones(isovals.size())) / (isovals.maxCoeff() - isovals.minCoeff());
-    igl::colormap(igl::COLOR_MAP_TYPE_MAGMA, isovals_normalized, false, C);
-    viewer.data().point_size = 5.0;
-    viewer.data().add_points(TV, C);
-    viewer.data().add_edges(v1, v2, Eigen::RowVector3d(0.1, 0.1, 0.1));
+  Eigen::MatrixXd v1(edges.size(), 3), v2(edges.size(), 3);
+  for (int i = 0; i < edges.size(); i++) {
+    v1.row(i) = TV.row(edges[i].first);
+    v2.row(i) = TV.row(edges[i].second);
+  }
+
+  // Normalize the isovalues between 0 and 1 for the colormap
+  Eigen::MatrixXd C;
+  const double isoval_min = isovals.minCoeff();
+  const double isoval_max = isovals.maxCoeff();
+  const double isoval_spread = isoval_max - isoval_min;
+  Eigen::VectorXd isovals_normalized =
+      (isovals - isoval_min * VectorXd::Ones(isovals.size())) / isoval_spread;
+
+  // Draw colored vertices of tet mesh based on their isovalue and black
+  // lines connecting the vertices
+  igl::colormap(igl::COLOR_MAP_TYPE_MAGMA, isovals_normalized, false, C);
+  viewer.data().point_size = 5.0;
+  viewer.data().add_points(TV, C);
+  viewer.data().add_edges(v1, v2, Eigen::RowVector3d(0.1, 0.1, 0.1));
 }
 
 
 int main(int argc, char *argv[]) {
-    using namespace Eigen;
-    using namespace std;
+  using namespace Eigen;
+  using namespace std;
 
-    // Load a surface mesh
-    igl::readOFF(TUTORIAL_SHARED_PATH "/fertility.off",V,F);
+  // Load a surface mesh
+  igl::readOFF(TUTORIAL_SHARED_PATH "/fertility.off",V,F);
 
-    // Tetrahedralize the interior
-    igl::copyleft::tetgen::tetrahedralize(V,F,"pq1.414Y", TV,TT,TF);
+  // Tetrahedralize the interior
+  igl::copyleft::tetgen::tetrahedralize(V,F,"pq1.414Y", TV,TT,TF);
 
-    // Make the isovalues of each tet vertex, be the distance form the origin
-    isovalues.resize(TV.rows());
-    for (int i = 0; i < isovalues.size(); i++) {
-        isovalues[i] = TV.row(i).norm();
-    }
+  // Make the isovalues of each tet vertex, be the distance form the origin
+  isovalues.resize(TV.rows());
+  for (int i = 0; i < isovalues.size(); i++) {
+    isovalues[i] = TV.row(i).norm();
+  }
 
-    // We'll compute the level set with this value
-    double level_set_value = 45.0;
+  // We'll compute the level set with this value
+  double level_set_value = 45.0;
 
-    igl::marching_tets(TV, TT, isovalues, level_set_value, isoV, isoF);
+  igl::marching_tets(TV, TT, isovalues, level_set_value, isoV, isoF);
 
-    // Plot the generated mesh
-    igl::opengl::glfw::Viewer viewer;
-    viewer.data().set_mesh(isoV, isoF);
-    visualize_tet_wireframe(viewer, TV, TT, isovalues);
-    viewer.launch();
+  // Plot the generated mesh
+  igl::opengl::glfw::Viewer viewer;
+  viewer.data().set_mesh(isoV, isoF);
+  visualize_tet_wireframe(viewer, TV, TT, isovalues);
+  viewer.launch();
 }
